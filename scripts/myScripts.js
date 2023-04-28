fa7ba09cd4674a74ce0858cec449257c689cf70d
Shane <g00391599@Atu.ie>
2023-04-28 15:30:25 +0100

    links title animation change + remove green check

--- scripts/myScripts.js
+++ scripts/myScripts.js
@@ -197,23 +197,6 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-document.addEventListener('DOMContentLoaded', () => {
-  const form = document.querySelector('.enquiry-form');
-  const checkmarkContainer = document.querySelector('.checkmark-container');
-
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    // Display the checkmark and hide the form
-    form.style.display = 'none';
-    checkmarkContainer.style.display = 'flex';
-
-    // Wait for 2 seconds before hiding the checkmark
-    setTimeout(() => {
-      checkmarkContainer.style.display = 'none';
-    }, 10000);
-  });
-});
 
 
 //PRODUCTS BOLTS PAGE END
